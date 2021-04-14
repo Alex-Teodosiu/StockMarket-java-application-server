eb2a82ca03169c331628151be0008843aafcd41f
lks1909 <lk.saldanha@gmail.com>
2021-04-14 11:01:42 +0200

    Initial commit

---
+++ src/main/java/com/sep3/javaapplicationserver/JavaApplicationServerApplication.java
@@ -0,0 +1,13 @@
+package com.sep3.javaapplicationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApplicationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaApplicationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sep3/javaapplicationserver/JavaApplicationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sep3.javaapplicationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
