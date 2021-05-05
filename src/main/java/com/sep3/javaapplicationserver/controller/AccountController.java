d85a188d83ea0d0f9d34c67e4ce0e1595a76cb8f
Pupendo <michal.pupak@gmail.com>
2021-05-05 18:19:13 +0200

    Error Message

--- src/main/java/com/sep3/javaapplicationserver/controller/AccountController.java
+++ src/main/java/com/sep3/javaapplicationserver/controller/AccountController.java
@@ -2,8 +2,12 @@ package com.sep3.javaapplicationserver.controller;
 
 import com.sep3.javaapplicationserver.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.sep3.javaapplicationserver.model.Account;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
 @RestController
 @RequestMapping("/account")
@@ -17,7 +21,15 @@ public class AccountController {
     }
 
     @PostMapping("")
-    public void addNewAccount(@RequestBody Account account) {
-        accountService.addNewAccount(account);
+    public ResponseEntity<String> addNewAccount(@RequestBody Account account) {
+        ResponseEntity<String> entity;
+        try {
+            accountService.addNewAccount(account);
+            entity = new ResponseEntity<>("ok",HttpStatus.OK);
+        }catch (Exception e){
+            entity = new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            System.out.println(entity.getBody());
+        }
+        return entity;
     }
 }
