5abb0b70a6c47ffcda9864985ac4f83b1d577b98
Pupendo <michal.pupak@gmail.com>
2021-05-20 09:35:18 +0200

    Fixed edit account

--- src/main/java/com/sep3/javaapplicationserver/service/AccountServiceImpl.java
+++ src/main/java/com/sep3/javaapplicationserver/service/AccountServiceImpl.java
@@ -2,12 +2,10 @@ package com.sep3.javaapplicationserver.service;
 
 import com.sep3.javaapplicationserver.model.Account;
 import com.sep3.javaapplicationserver.repository.AccountRepository;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -36,33 +34,9 @@ public class AccountServiceImpl implements AccountService {
         Optional<Account> accountOptional = accountRepository
                 .findAccountByUsername(account.getUsername());
 
-        if(accountOptional.isPresent()) {
+        if(accountOptional.isPresent() && !account.getId().equals(accountOptional.get().getId())) {
             throw new DataIntegrityViolationException("Username already taken");
         }
-//        BeanUtils.copyProperties(account, accountOptional.get(), "id", "dateCreated");
-//        accountOptional.get().setDateUpdated(LocalDateTime.now());
-//
-//        return accountRepository.save(accountOptional.get());
-
-
-//        Optional<Account> accountOptional = accountRepository
-//                    .findAccountByUsername(account.getUsername());
-//
-//        if (accountOptional.isPresent()) {
-//            throw new DataIntegrityViolationException("Username already taken");
-//        }
-//
-//        if(account.getUsername()!= null && account.getUsername().length()>0
-//                && !Objects.equals(accountOptional.get().getUsername(), account.getUsername())){
-//            accountOptional.get().setUsername(account.getUsername());
-//        }
-//
-//        if(account.getPassword()!=null && account.getPassword().length()>6
-//                && !Objects.equals(accountOptional.get().getPassword(), account.getPassword())){
-//            accountOptional.get().setPassword(account.getPassword());
-//        }
-//
-//        return accountRepository.save(accountOptional.get());
     }
 
 }
